b1984a2be79ba0b7574b216c1f6cb08da3f867dd
Florian Perucki <florian@perucki.fr>
2014-10-19 15:36:27 +0200

    Remove unused imports and methods

--- server/src/com/dar/metier/DBHandler.java
+++ server/src/com/dar/metier/DBHandler.java
@@ -1,13 +1,10 @@
 package com.dar.metier;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import com.dar.beans.User;
-
 public class DBHandler {
-	//private static final String dbPath = getServletContext().getRealPath("/") + "/WEB-INF/dar.db";
+	
 	private static Connection connection = null;
 	private static String uri = "jdbc:sqlite:";
 	private static String dbPath;
@@ -44,27 +41,4 @@ public class DBHandler {
 			e.printStackTrace();
 		}
 	}
-	
-	public String getSeenMovies(User user){
-		// TODO
-		return "";
-	}
-	
-	public String getInfosOnMovie(User user, int movie_id){
-		// TODO
-		return "";
-	}
-	
-	public boolean addSeenMovie(User user, String title){
-		// TODO
-		// if no movie with title "title" exists in db.movie, create an entry
-		// 
-		// add entry (user.login, movie.id, cine.id) in db.movie_seen  
-		return false;
-	}
-	
-//	public boolean saveUserLocation(User user, Location loc){
-//		// TODO
-//		return false;
-//	}
 }
