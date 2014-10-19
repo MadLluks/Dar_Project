5cf08ef6a69a3774b3d0ede00fc88846b35e3b1b
Florian Perucki <florian@perucki.fr>
2014-10-19 13:15:40 +0200

    Add SQL table creation script

---
+++ server/WebContent/WEB-INF/database_create_tables.sql
@@ -0,0 +1,48 @@
+
+
+CREATE TABLE user
+(
+	login varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	PRIMARY KEY (login)
+);
+
+CREATE TABLE location
+(
+	loc_id int NOT NULL,
+	lat float NOT NULL,
+	lon float NOT NULL,
+	address varchar(255),
+	postal_code varchar(255),
+	city varchar(255),
+	PRIMARY KEY (loc_id)
+);
+
+
+CREATE TABLE cinema
+(
+	cine_id int NOT NULL,
+	loc_id int NOT_NULL,
+	PRIMARY KEY (cine_id),
+	FOREIGN KEY (loc_id) REFERENCES location(loc_id)
+);
+
+CREATE TABLE movie
+(
+	movie_id int NOT NULL,
+	title varchar(255) NOT NULL,
+	PRIMARY KEY (movie_id)
+);
+
+CREATE TABLE movie_seen
+(
+	movie_id int NOT NULL,
+	cine_id int NOT NULL,
+	login varchar(255) NOT NULL,
+	FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+	FOREIGN KEY (cine_id) REFERENCES cinema(cine_id),
+	FOREIGN KEY (login) REFERENCES user(login),
+	CONSTRAINT u_movie_seen UNIQUE (movie_id, cine_id, login)
+);
+
+
