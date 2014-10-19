7606079b5d958ddad2943f1b92784a50a7602b14
Florian Perucki <florian@perucki.fr>
2014-10-19 19:28:36 +0200

    Add methods to save and to get movies

--- server/src/com/dar/beans/User.java
+++ server/src/com/dar/beans/User.java
@@ -2,15 +2,21 @@ package com.dar.beans;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.dar.metier.DBHandler;
 
 public class User {
 	private String login;	
 	private String password;
-	Connection conn;
-
+	private Connection conn;
+	private ArrayList<Movie> seenMovies = null;
+	private boolean movieAdded = false;
+	private boolean passwordModified = false;
+	private boolean registered = false;
+	
 	public User(String login, String password){
 		this.login = login;
 		this.password = password;
@@ -25,10 +31,6 @@ public class User {
 	
 	public String getLogin() {
 		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
 	}
 	
 	public String getPassword(){
@@ -47,6 +49,7 @@ public class User {
 			prestmt.setString(1, login);
 			prestmt.setString(2, password);
 			prestmt.execute();
+			registered = true;
 		}
 		catch(SQLException e){
 			e.printStackTrace();
@@ -64,17 +67,72 @@ public class User {
 		boolean res;
 		try {
 			prestmt = this.conn
-					.prepareStatement("select * from user where login = ? and password = ?");
+					.prepareStatement("SELECT * FROM user WHERE login = ? AND password = ?");
 			prestmt.setString(1, login);
 			prestmt.setString(2, password);
 			res = prestmt.executeQuery().next();
 			this.login = login;
 			this.password = password;
+			this.registered = true;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			res = false;
 		}
 		return res;
+	}
+	
+	public ArrayList<Movie> getSeenMovies(){
+		if(seenMovies == null || movieAdded){
+			movieAdded = false;
+			seenMovies = new ArrayList<Movie>();
+			PreparedStatement prestmt;
+			try {
+				String query = "SELECT m.movie_id id,m.title title FROM movie m, movie_seen ms, user u ";
+				query += "WHERE m.movie_id = ms.movie_id AND ms.login = u.login ";
+				query += "AND login = ?";
+				prestmt = this.conn.prepareStatement(query);
+				prestmt.setString(1, login);
+				ResultSet res = prestmt.executeQuery();
+				while(res.next()){
+					String id = res.getString("movie_id");
+					String title = res.getString("title");
+					seenMovies.add(new Movie(id, title));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return seenMovies;
+	}
+	
+	public boolean addSeenMovie(Movie m){
+		this.movieAdded = true;
+		this.seenMovies.add(m);
+		return m.save();
+	}
+	
+	public boolean save(){
+		boolean success = false;
+		if(!this.registered)
+			success = this.register();
+		if(movieAdded){
+			for(Movie m : this.seenMovies){
+				success &= m.save();
+			}
+		}
+		if(passwordModified){
+			String query = "UPDATE user SET password = ? WHERE login = ?";
+			PreparedStatement prestmt;
+			try {				
+				prestmt = this.conn.prepareStatement(query);
+				prestmt.setString(1, login);
+				prestmt.executeQuery();				
+			} catch (SQLException e) {
+				e.printStackTrace();
+				success = false;
+			}
+		}		
+		return success;
 	}
 	
 }
