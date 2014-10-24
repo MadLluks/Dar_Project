abc2405bc1c4879974c2169104ef033fda8ae072
Florian Perucki <florian@perucki.fr>
2014-10-24 12:15:18 +0200

    Refactor as maven project deployable to heroku

---
+++ server/src/main/java/metier/DBHandler.java
@@ -0,0 +1,46 @@
+package main.java.metier;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBHandler {
+	
+	private static Connection connection = null;
+	private static String uri = "jdbc:sqlite:";
+	private static String dbPath;
+	
+	private DBHandler(){}
+
+	/*
+	 * Called by InitContextListener
+	 */
+	public static void setPath(String path){
+		dbPath = path;
+	}
+	
+	public static String getPath(){
+		return dbPath;
+	}
+	
+	public static Connection getInstance(){
+		if(connection == null){
+			try {
+				Class.forName("org.sqlite.JDBC");
+				connection = DriverManager.getConnection(uri+dbPath);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch(ClassNotFoundException e){
+				e.printStackTrace();
+			}
+		}		
+		return connection;	
+	}
+	
+	public void close() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ server/src/main/java/servlet/MovieServlet.java
@@ -0,0 +1,107 @@
+package main.java.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import main.java.beans.Cinema;
+import main.java.beans.Movie;
+import main.java.beans.User;
+
+/**
+ * Servlet implementation class MovieServlet
+ */
+@WebServlet("/MovieServlet")
+public class MovieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MovieServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		// Get movies saved by current user
+		User user = (User) request.getSession().getAttribute("user");
+		if(user != null){
+			String jsonResp = "";
+			ArrayList<Movie> movies = user.getSeenMovies();
+			for(Movie m : movies){
+				jsonResp += m.toJson()+", ";
+			}
+			jsonResp = "{\"success\": true, \"result\": "+jsonResp+"}";
+			PrintWriter out = response.getWriter();
+			out.print(jsonResp);
+			out.flush();
+			out.close();
+		}
+		else{
+			PrintWriter out = response.getWriter();
+			out.print("{\"success\": false, \"error\": \"not_logged_in\"}");
+			out.flush();
+			out.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Save movie for current user
+				
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");		
+		// user must be logged in
+		User user = (User) request.getSession().getAttribute("user");
+		if(user == null){
+			out.print("{\"success\" : false, \"error\" : \"login_required\"}");
+			out.flush();
+			out.close();
+			return;
+		}
+		String title;
+		Float cine_lon, cine_lat;
+		String movie_id;
+		String name;
+		try{
+			title = request.getParameter("title");
+			movie_id = request.getParameter("movie_id");
+			cine_lon = Float.valueOf(request.getParameter("cine_lon"));
+			cine_lat = Float.valueOf(request.getParameter("cine_lat"));
+			name = request.getParameter("cine_name");
+			if(name == null || cine_lat == null || cine_lon == null || title == null || movie_id == null)
+				throw new Exception(); 
+		}
+		catch(Exception e){
+			out.print("{\"success\" : false, \"error\" : \"missing_parameter\"}");
+			out.flush();
+			out.close();
+			return;
+	    }
+		
+		Cinema cine = new Cinema(name, cine_lat, cine_lon);
+		Movie m = new Movie(movie_id, title);
+		
+		if(!user.addSeenMovie(m,cine) || !user.save()){
+			out.print("{\"success\": false, \"error\": \"user_save_error\"}");
+			out.flush();
+		}
+		out.print("{\"success\" : true}");
+		out.flush();
+		out.close();
+		
+	}
+
+}
